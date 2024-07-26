8cd75fad27c5ed75b2e7f5172e9eb9880417ef18
GillesNorce <gipe@norceresearch.no>
2024-07-26 12:28:40 +0200

    Trying something else

--- WebApp/Shared/APIUtils.cs
+++ WebApp/Shared/APIUtils.cs
@@ -1,10 +1,12 @@
-﻿public static class APIUtils
+﻿using OSDC.UnitConversion.ModelShared;
+
+public static class APIUtils
 {
     // API parameters
     public static readonly string HostNameUnitConversion = OSDC.UnitConversion.WebApp.Configuration.UnitConversionHostURL!;
     public static readonly string HostBasePathUnitConversion = "UnitConversion/api/";
     public static readonly HttpClient HttpClientUnitConversion = APIUtils.SetHttpClient(HostNameUnitConversion, HostBasePathUnitConversion);
-    public static readonly OSDC.UnitConversion.ModelShared.Client ClientUnitConversion = new(APIUtils.HttpClientUnitConversion.BaseAddress!.ToString(), APIUtils.HttpClientUnitConversion);
+    public static readonly Client ClientUnitConversion = new(APIUtils.HttpClientUnitConversion.BaseAddress!.ToString(), APIUtils.HttpClientUnitConversion);
 
     // API utility methods
     public static HttpClient SetHttpClient(string host, string microServiceUri)
