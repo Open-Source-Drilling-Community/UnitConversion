caa0de7d0de7f4d7d4d8ac606ebd20ebd66889f6
GillesNorce <gipe@norceresearch.no>
2024-07-26 14:46:30 +0200

    Adding webpage UnitConversionSetMain.razor

--- WebApp/Shared/DataUtils.cs
+++ WebApp/Shared/DataUtils.cs
@@ -1,18 +1,6 @@
 ﻿public static class DataUtils
 {
     // default values
-    public const double DEFAULT_VALUE = 999.25;
-    public static string DEFAULT_NAME_DataUnitConversionSet = "Default DataUnitConversionSet Name";
-    public static string DEFAULT_DESCR_DataUnitConversionSet = "Default DataUnitConversionSet Description";
-    public static string DEFAULT_NAME_QuantityUnitConversion = "Default QuantityUnitConversion Name";
-    public static string DEFAULT_DESCR_QuantityUnitConversion = "Default QuantityUnitConversion Description";
-
-    //DataUnitConversionSet parameters units
-    public static readonly string DataUnitConversionSetInputParamListLabel = "DataUnitConversionSetInputParamList";
-    public static readonly string DataUnitConversionSetOutputParamLabel = "DataUnitConversionSetOutputParam";
-    public static readonly string DataUnitConversionSetOutputParamQty = "Depth";
-    public static readonly string OutputXValuesTitle = "Some depth value";
-    public static readonly string OutputXValuesQty = "Depth";
-    public static readonly string OutputYValuesTitle = "Some angle value";
-    public static readonly string OutputYValuesQty = "DrillingPlaneAngle";
+    public static readonly string DEFAULT_NAME_UnitConversionSet = "Default UnitConversionSet Name";
+    public static readonly string DEFAULT_DESCR_UnitConversionSet = "Default UnitConversionSet Description";
 }
