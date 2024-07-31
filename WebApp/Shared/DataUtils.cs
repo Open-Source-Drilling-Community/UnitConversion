d7536e2de0feb87517d40e919e92a6badd13a327
GillesNorce <gipe@norceresearch.no>
2024-07-31 18:27:16 +0200

    Adding CultureInfo option

--- WebApp/Shared/DataUtils.cs
+++ WebApp/Shared/DataUtils.cs
@@ -1,6 +1,19 @@
-﻿public static class DataUtils
+﻿using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+public static class DataUtils
 {
     // default values
     public static readonly string DEFAULT_NAME_UnitConversionSet = "Default UnitConversionSet Name";
     public static readonly string DEFAULT_DESCR_UnitConversionSet = "Default UnitConversionSet Description";
+
+    public static readonly CultureInfo CULTURE_EN_US;
+    public static readonly CultureInfo CULTURE_EN_US_WO_SEP;
+
+    static DataUtils()
+    {
+        CULTURE_EN_US = new CultureInfo("en-US");
+        CULTURE_EN_US_WO_SEP = new CultureInfo("en-US");
+        CULTURE_EN_US_WO_SEP.NumberFormat.NumberGroupSeparator = "";
+    }
 }
