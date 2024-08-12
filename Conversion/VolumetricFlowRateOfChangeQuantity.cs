60cfaa2a260fa03bd87a2f9290e9776f9b2471ae
EricCayeux <eric.cayeux@norceresearch.no>
2024-08-12 16:01:16 +0200

    Added unit tests and used factors for volumetric flow rate of change

--- Conversion/VolumetricFlowRateOfChangeQuantity.cs
+++ Conversion/VolumetricFlowRateOfChangeQuantity.cs
@@ -1,6 +1,8 @@
+using Conversion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace OSDC.UnitConversion.Conversion
 {
@@ -37,57 +39,57 @@ namespace OSDC.UnitConversion.Conversion
           UnitName = SIUnitName,
           UnitLabel = SIUnitLabel,
           ID = new Guid("aef20431-be0b-44ea-8770-a59db19b7f94"),
-          ConversionFactorFromSI = 1.0,
+          ConversionFactorFromSI = 1.0/Factors.Unit,
           IsSI = true
         },
         new UnitChoice
         {
           UnitName = "litre per minute squared",
-          UnitLabel = "l/min²",
+          UnitLabel = "L/min²",
           ID = new Guid("b27d2f54-a1f3-4abb-ba6d-a2a8b530049a"),
-          ConversionFactorFromSI = 3600000
+          ConversionFactorFromSI = Factors.Minute*Factors.Minute/Factors.Milli
         },
         new UnitChoice
         {
           UnitName = "litre per minute per second",
-          UnitLabel = "l/min/s",
+          UnitLabel = "L/min/s",
           ID = new Guid("e5a265b6-a9ba-4a09-ba08-b8c417b28ffb"),
-          ConversionFactorFromSI = 60000
+          ConversionFactorFromSI = Factors.Minute/Factors.Milli
         },
         new UnitChoice
         {
           UnitName = "litre per second squared",
-          UnitLabel = "l/s²",
+          UnitLabel = "L/s²",
           ID = new Guid("a899c06f-18dd-4d2a-9743-489f0af5be91"),
-          ConversionFactorFromSI = 1000
+          ConversionFactorFromSI = 1.0/Factors.Milli
         },
         new UnitChoice
         {
           UnitName = "UK gallon per minute squared",
           UnitLabel = "UKGal/min²",
           ID = new Guid("c7c61175-e527-4403-8425-32f681367985"),
-          ConversionFactorFromSI = 791889.293874
+          ConversionFactorFromSI = Factors.Minute*Factors.Minute/Factors.GallonUK
         },
         new UnitChoice
         {
           UnitName = "UK gallon per minute per second",
           UnitLabel = "UKGal/min/s",
           ID = new Guid("298e7a16-07a5-4b5b-a0de-3e49b31254b4"),
-          ConversionFactorFromSI = 13198.1549
+          ConversionFactorFromSI = Factors.Minute*Factors.Unit/Factors.GallonUK
         },
         new UnitChoice
         {
           UnitName = "US gallon per minute squared",
           UnitLabel = "USGal/min²",
           ID = new Guid("61885289-823d-4b26-bdf2-bc4744567bef"),
-          ConversionFactorFromSI = 951019.38447
+          ConversionFactorFromSI = Factors.Minute*Factors.Minute/Factors.GallonUS
         },
         new UnitChoice
         {
           UnitName = "US gallon per minute per second",
           UnitLabel = "USGal/min/s",
           ID = new Guid("3c530e9a-9376-49d1-a6b5-0a6f93f4184b"),
-          ConversionFactorFromSI = 15850.32307
+          ConversionFactorFromSI = Factors.Minute*Factors.Unit/Factors.GallonUS
         }
       };
         }
