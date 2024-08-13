37c6d656d91a01ea8d606909b982f66774081c46
EricCayeux <eric.cayeux@norceresearch.no>
2024-08-13 15:56:58 +0200

    added unit tests and used factors for temperature gradient

--- Conversion/TemperatureGradientQuantity.cs
+++ Conversion/TemperatureGradientQuantity.cs
@@ -1,3 +1,4 @@
+using Conversion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,7 +38,7 @@ namespace OSDC.UnitConversion.Conversion
           UnitName = SIUnitName,
           UnitLabel = SIUnitLabel,
           ID = new Guid("f1fe19d2-12e3-43d1-ba97-3ef9e8ec9e73"),
-          ConversionFactorFromSI = 1.0,
+          ConversionFactorFromSI = 1.0/Factors.Unit,
           IsSI = true
         },
         new UnitChoice
@@ -45,70 +46,70 @@ namespace OSDC.UnitConversion.Conversion
           UnitName = "celsius per metre",
           UnitLabel = "°C/m",
           ID = new Guid("40dbbdfe-b680-403a-8326-2c217ba85d52"),
-          ConversionFactorFromSI = 1
+          ConversionFactorFromSI = 1.0/Factors.Unit
         },
         new UnitChoice
         {
           UnitName = "celsius per 10 metre",
           UnitLabel = "°C/10m",
           ID = new Guid("5e4ff2bf-4788-4258-bd4a-7b18a13364ff"),
-          ConversionFactorFromSI = 10
+          ConversionFactorFromSI = Factors.Deca/Factors.Unit
         },
         new UnitChoice
         {
           UnitName = "celsius per 30 metre",
           UnitLabel = "°C/30m",
           ID = new Guid("d17464c4-a7ef-4dcd-b783-bafe6e9b92de"),
-          ConversionFactorFromSI = 30
+          ConversionFactorFromSI = 3.0*Factors.Deca/Factors.Unit
         },
         new UnitChoice
         {
           UnitName = "celsius per 100 metre",
           UnitLabel = "°C/100m",
           ID = new Guid("b47f299a-913a-46b7-ad20-c683fa0f02d0"),
-          ConversionFactorFromSI = 100
+          ConversionFactorFromSI = Factors.Hecto/Factors.Unit
         },
         new UnitChoice
         {
           UnitName = "celsius per foot",
           UnitLabel = "°C/ft",
           ID = new Guid("e7b05420-41f6-4812-bc54-9c14f05a9cbd"),
-          ConversionFactorFromSI = 0.3048
+          ConversionFactorFromSI = Factors.Foot/Factors.Unit
         },
         new UnitChoice
         {
           UnitName = "celsius per 30 foot",
           UnitLabel = "°C/30ft",
           ID = new Guid("bea3df4f-78e9-4e1a-bbee-22086da043b4"),
-          ConversionFactorFromSI = 9.144
+          ConversionFactorFromSI = 30.0*Factors.Foot/Factors.Unit
         },
         new UnitChoice
         {
           UnitName = "celsius per 100 foot",
           UnitLabel = "°C/100ft",
           ID = new Guid("f9bae95a-b282-44a7-8ae0-54728ef3c7a3"),
-          ConversionFactorFromSI = 30.48
+          ConversionFactorFromSI = 100.0*Factors.Foot/Factors.Unit
         },
         new UnitChoice
         {
           UnitName = "fahrenheit per foot",
           UnitLabel = "°F/ft",
           ID = new Guid("d08596f1-77c4-4a8e-9245-6bf563fa7345"),
-          ConversionFactorFromSI = 0.54864
+          ConversionFactorFromSI = Factors.Foot/Factors.FahrenheitSlope
         },
         new UnitChoice
         {
           UnitName = "fahrenheit per 30 foot",
           UnitLabel = "°F/30ft",
           ID = new Guid("a1664cb0-db5c-4933-9b57-d075c4975f46"),
-          ConversionFactorFromSI = 16.4592
+          ConversionFactorFromSI = 30.0*Factors.Foot/Factors.FahrenheitSlope
         },
         new UnitChoice
         {
           UnitName = "fahrenheit per 100 foot",
           UnitLabel = "°F/100ft",
           ID = new Guid("232e2d6d-cb65-4b56-9277-457e4ff678fa"),
-          ConversionFactorFromSI = 54.864000000000004
+          ConversionFactorFromSI = 100.0*Factors.Foot/Factors.FahrenheitSlope
         }
       };
         }
